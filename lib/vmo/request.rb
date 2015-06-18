e1f6e03c4bd0538ed027fda894b4d095ef2935f1
MattHamada <mattahamada@gmail.com>
2015-06-18 13:44:40 -0700

    fixed misc bugs

--- lib/vmo/request.rb
+++ lib/vmo/request.rb
@@ -2,7 +2,8 @@ module Vmo
   class Request < Vmo::Base
     BASE_URL = 'https://api.vimeo.com'
 
-    def self.get_user
+    def self.get_user(token)
+      @token = token
       response = JSON.parse(get_request('/me').body)
       name = response['name']
       link = response['link']
@@ -14,7 +15,7 @@ module Vmo
         vid_name = video['name']
         vid_description = video['description']
         vid_created_time = video['created_time']
-        vid_embed_url = /https:\/\/player.vimeo.com\/video\/(\d+)/.match(video[0]['embed']['html'])[0]
+        vid_embed_url = /https:\/\/player.vimeo.com\/video\/(\d+)/.match(video['embed']['html'])[0]
         vnew = Vmo::Video.new(vid_id, vid_name, vid_description, vid_created_time, vid_embed_url)
         videos << vnew
       end
@@ -32,8 +33,8 @@ module Vmo
         options = auth_options(options)
         HTTParty.get(BASE_URL + url, options)
       end
-      def auth_options(options)
-        options['Authorization'] = "Bearer #{@token}"
+      def self.auth_options(options)
+        options[:headers] = {'Authorization' => "Bearer #{@token}" }
         options
       end
   end
