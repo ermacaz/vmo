d610542d4a743873e322b54a039ae381a9e44c4a
MattHamada <mattahamada@gmail.com>
2015-06-18 14:00:48 -0700

    added video link

--- lib/vmo/request.rb
+++ lib/vmo/request.rb
@@ -13,10 +13,11 @@ module Vmo
       vid_response.each do |video|
         vid_id = /(\d+)/.match(video['uri'])[0]
         vid_name = video['name']
+        vid_link = video['link']
         vid_description = video['description']
         vid_created_time = video['created_time']
         vid_embed_url = /https:\/\/player.vimeo.com\/video\/(\d+)/.match(video['embed']['html'])[0]
-        vnew = Vmo::Video.new(vid_id, vid_name, vid_description, vid_created_time, vid_embed_url)
+        vnew = Vmo::Video.new(vid_id, vid_name, vid_description, vid_created_time, vid_embed_url, vid_link)
         videos << vnew
       end
       Vmo::User.new(id, name, link, videos)
--- lib/vmo/video.rb
+++ lib/vmo/video.rb
@@ -1,12 +1,13 @@
 module Vmo
   class Video < Vmo::Base
-    attr_accessor :id, :name, :description, :created_time, :embedUrl
-    def initialize(id, name, description, created_time, url)
+    attr_accessor :id, :name, :description, :created_time, :embedUrl, :link
+    def initialize(id, name, description, created_time, embedUrl, link)
       @id = id
       @name = name
       @description = description
       @created_time = created_time
-      @embedUrl = url
+      @embedUrl = embedUrl
+      @link = link
     end
   end
 end
